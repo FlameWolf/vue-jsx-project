cf05973e6bf39241baaa86f7f679f22541ee4557
FlameWolf <agnijith@gmail.com>
2026-09-04 19:26:38 +0530

    Set `displayName` for using as route component

--- src/components/PrivacyPolicy.tsx
+++ src/components/PrivacyPolicy.tsx
@@ -1,6 +1,8 @@
 import { privacyEffectiveDate, privacyIntro, privacySections } from "@/content/privacy";
 import LegalPage from "@/components/LegalPage";
 
-export default function PrivacyPolicy() {
+function PrivacyPolicy() {
 	return <LegalPage title="Privacy Policy" effectiveDate={privacyEffectiveDate} intro={privacyIntro} sections={privacySections}/>;
 }
+
+export default Object.assign(PrivacyPolicy, { displayName: PrivacyPolicy.name });
--- src/components/TermsOfService.tsx
+++ src/components/TermsOfService.tsx
@@ -1,6 +1,8 @@
 import { termsEffectiveDate, termsIntro, termsSections } from "@/content/terms";
 import LegalPage from "@/components/LegalPage";
 
-export default function TermsOfService() {
+function TermsOfService() {
 	return <LegalPage title="Terms of Service" effectiveDate={termsEffectiveDate} intro={termsIntro} sections={termsSections}/>;
 }
+
+export default Object.assign(TermsOfService, { displayName: TermsOfService.name });
